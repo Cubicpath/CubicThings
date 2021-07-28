91391178c200edbb5a1fbf1eccdf94ea22fd5fcb
Cubicpath <Cubicpath@protonmail.com>
2021-07-28 01:27:21 -0400

    Config improvements

--- src/main/java/com/cubicpath/cubicthings/CubicThings.java
+++ src/main/java/com/cubicpath/cubicthings/CubicThings.java
@@ -7,6 +7,7 @@ package com.cubicpath.cubicthings;
 import com.cubicpath.cubicthings.client.gui.screen.ConfigScreen;
 import com.cubicpath.cubicthings.core.init.*;
 
+import com.electronwill.nightconfig.core.AbstractConfig;
 import com.google.common.collect.Lists;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -41,29 +43,55 @@ public final class CubicThings {
     @SuppressWarnings("unused")
     public static final class Config {
         public static final ForgeConfigSpec SPEC;
-        public static ForgeConfigSpec.ConfigValue<String> stringValue;
+        public static ForgeConfigSpec.ConfigValue<Boolean> booleanValue;
+        public static ForgeConfigSpec.ConfigValue<Integer> byteValue;
         public static ForgeConfigSpec.ConfigValue<Integer> integerValue;
         public static ForgeConfigSpec.ConfigValue<Double> doubleValue;
-        public static ForgeConfigSpec.ConfigValue<Boolean> booleanValue;
+        public static ForgeConfigSpec.ConfigValue<String> stringValue;
+
+        public static ForgeConfigSpec.ConfigValue<Boolean> bytesAsBinary;
+        public static ForgeConfigSpec.ConfigValue<Integer> fail;
+        public static ForgeConfigSpec.ConfigValue<Integer> good;
 
         static {
             ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-            buildValue(builder, "stringValue", "Value", "This is a String value. It has quotation marks.", String.class);
-            buildValue(builder, "integerValue", 0, "This is an Integer value. It is a whole number.", Integer.class);
-            buildValue(builder, "doubleValue", 1.000D, "This is a Double value. It has decimals.", Double.class);
-            buildValue(builder, "booleanValue", true, "This is a Boolean value. It has 2 possible values.", Boolean.class);
+            buildValue(Config.class, builder, "booleanValue", true, "This is a Boolean value. It has 2 possible values.", Boolean.class);
+            buildValue(Config.class, builder, "byteValue", (byte) 0, "This is an Integer value. It is a whole number.", Byte.class);
+            buildValue(Config.class, builder, "integerValue", 0, "This is an Integer value. It is a whole number.", Integer.class);
+            buildValue(Config.class, builder, "doubleValue", 1.000D, "This is a Double value. It has decimals.", Double.class);
+            buildValue(Config.class, builder, "stringValue", "Value", "This is a String value. It has quotation marks.", String.class);
+            builder.push("configMenu");
+            buildValue(Config.class, builder, "bytesAsBinary", true, "Show byte config values as binary strings.", Boolean.class);
+            builder.push("color");
+            buildValue(Config.class, builder, "fail", 0xBB4444, null, Integer.class);
+            buildValue(Config.class, builder, "good", 0x44BA44, "", Integer.class);
+            builder.pop(2);
             SPEC = builder.build();
         }
 
-        static <T> void buildValue(ForgeConfigSpec.Builder builder, String valueName, T defaultValue, @Nullable String comment, Class<T> clazz){
-            List<String> path = Lists.newArrayList(valueName);
-            Supplier<T> defaultSupplier = () -> defaultValue;
-            Predicate<Object> validator = (o) -> o != null && clazz.isAssignableFrom(o.getClass());
+        static <T> void buildValue(Class<?> configClazz, ForgeConfigSpec.Builder builder, String fieldName, T defaultValue, @Nullable String comment, Class<T> valueClazz){
             try {
+                List<String> path = Lists.newArrayList(fieldName);
+                Supplier<T> defaultSupplier = () -> defaultValue;
+                Predicate<Object> validator = (o) -> o != null && valueClazz.isAssignableFrom(o.getClass());
                 if (comment != null) builder.comment(comment);
-                Config.class.getField(valueName).set(null, builder.translation("config." + MODID + "." + valueName).define(path, defaultSupplier, validator, clazz));
+                configClazz.getField(fieldName).set(null, builder.translation("config." + MODID + "." + fieldName).define(path, defaultSupplier, validator, valueClazz));
             } catch (NoSuchFieldException | IllegalAccessException exception){
-                LOGGER.warn("Unable instantiate configValue '" + valueName + "' using reflection. Reason: " + exception.getMessage());
+                LOGGER.warn("Unable to instantiate configValue '" + fieldName + "' using reflection. Reason: " +  exception);
+            }
+        }
+
+        public static boolean isValueSpecInConfigSpec(ForgeConfigSpec spec, String[] path){
+            return isValueSpecInPath(spec.valueMap(), path, 0);
+        }
+
+        private static boolean isValueSpecInPath(Map<String, Object> valueMap, String[] path, final int depth){
+            try {
+                Object o = valueMap.get(path[depth]);
+                AbstractConfig config = o instanceof AbstractConfig ? (AbstractConfig) o : null;
+                return (o instanceof ForgeConfigSpec.ValueSpec && depth == path.length - 1) || (config != null && isValueSpecInPath(config.valueMap(), path, depth + 1));
+            } catch (ArrayIndexOutOfBoundsException e){
+                return false;
             }
         }
     }
--- src/main/java/com/cubicpath/cubicthings/client/gui/screen/ConfigScreen.java
+++ src/main/java/com/cubicpath/cubicthings/client/gui/screen/ConfigScreen.java
@@ -7,6 +7,7 @@ package com.cubicpath.cubicthings.client.gui.screen;
 import com.cubicpath.cubicthings.CubicThings;
 import com.cubicpath.cubicthings.client.gui.widget.TextListWidget;
 
+import com.electronwill.nightconfig.core.AbstractConfig;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
@@ -17,14 +18,17 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.StringUtils;
 
+import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class ConfigScreen extends Screen implements ITextListHolder {
     protected static boolean hideComments, hideSeparators = false;
 
-    protected boolean clickedKeyField, clickedValueField, invalidKey, invalidValue = false;
+    protected boolean clickedKeyField = false, clickedValueField = false, invalidKey = false, invalidValue = false, foundKey = false;
     protected final Screen prevScreen;
     protected Button cancelButton, toggleCommentsButton, toggleSeparatorsButton, submitChangeButton;
     protected TextFieldWidget keyInputField, valueInputField;
@@ -50,8 +54,8 @@ public class ConfigScreen extends Screen implements ITextListHolder {
             this.clickedValueField = true;
         }
 
-        if (CubicThings.Config.SPEC.valueMap().containsKey(this.keyInputField.getText()))
-            this.keyInputField.setTextColor(0x44BA44);
+        if (!this.keyInputField.getText().trim().endsWith(".") && CubicThings.Config.isValueSpecInConfigSpec(CubicThings.Config.SPEC, this.keyInputField.getText().trim().split("\\.")))
+            this.keyInputField.setTextColor(CubicThings.Config.good.get());
         else if (!this.invalidKey)
             this.keyInputField.setTextColor(0xA0A0A0);
 
@@ -68,30 +72,122 @@ public class ConfigScreen extends Screen implements ITextListHolder {
 
     @Override
     public <T extends ExtendedList.AbstractListEntry<T>> void buildTextList(Consumer<T> textListViewConsumer, BiFunction<String, Integer, T> newEntry) {
-        CubicThings.Config.SPEC.valueMap().keySet().forEach((key) -> {
-            if (!hideComments) textListViewConsumer.accept(newEntry.apply("#" + ((ForgeConfigSpec.ValueSpec)CubicThings.Config.SPEC.valueMap().get(key)).getComment(), 0x8190A0));
-            try {
-                Object o = ((ForgeConfigSpec.ConfigValue<?>)CubicThings.Config.class.getField(key).get(null)).get();
-                if (o instanceof String)
-                    textListViewConsumer.accept(newEntry.apply(key + "\u00A77 = \u00A72\"" + ((String) o).replace("\\", "\\\\").replace("\"", "\\\"") + "\"", 0xDB7F41));
-                else if (o instanceof Boolean)
-                    textListViewConsumer.accept(newEntry.apply(key + "\u00A77 = \u00A7d" + o, 0xDB7F41));
-                else if (o instanceof Integer)
-                    textListViewConsumer.accept(newEntry.apply(key + "\u00A77 = \u00A76" + o, 0xDB7F41));
-                else
-                    textListViewConsumer.accept(newEntry.apply(key + "\u00A77 = \u00A76" + o.toString(), 0xDB7F41));
-            } catch (IllegalAccessException | NoSuchFieldException ignored) { }
-            if (!hideSeparators) textListViewConsumer.accept(newEntry.apply("--------------------", 0x4400AA));
+        addTextEntries(CubicThings.Config.SPEC.valueMap(), textListViewConsumer, newEntry);
+    }
+
+    public <T extends ExtendedList.AbstractListEntry<T>> void addTextEntries(Map<String, Object> valueMap, Consumer<T> textListViewConsumer, BiFunction<String, Integer, T> newEntry){
+        addTextEntries(valueMap, textListViewConsumer, newEntry, null, 0);
+    }
+
+    private <T extends ExtendedList.AbstractListEntry<T>> void addTextEntries(Map<String, Object> valueMap, Consumer<T> textListViewConsumer, BiFunction<String, Integer, T> newEntry, @Nullable String prevConfigName, final int depth){
+        valueMap.keySet().forEach((key) -> {
+            String pathName = prevConfigName != null ? prevConfigName + "." + key : key;
+            if (valueMap.get(key) instanceof AbstractConfig) {
+                AbstractConfig abstractConfig = (AbstractConfig) valueMap.get(key);
+                //if (!hideComments) textListViewConsumer.accept(newEntry.apply(StringUtils.repeat(' ', depth) + "#" + abstractConfig.contains(key), 0x8190A0));
+                textListViewConsumer.accept(newEntry.apply(StringUtils.repeat(' ', depth * 4) + "\u00A77[\u00A7r" + pathName + "\u00A77]", 0xDB7F41));
+                addTextEntries(abstractConfig.valueMap(), textListViewConsumer, newEntry, pathName, depth + 1);
+            } else {
+                if (!hideComments && valueMap.get(key) instanceof ForgeConfigSpec.ValueSpec && ((ForgeConfigSpec.ValueSpec)valueMap.get(key)).getComment() != null && !((ForgeConfigSpec.ValueSpec)valueMap.get(key)).getComment().isEmpty())
+                    textListViewConsumer.accept(newEntry.apply(StringUtils.repeat(' ', depth * 4 - 2) + "#" + ((ForgeConfigSpec.ValueSpec)valueMap.get(key)).getComment(), 0x8190A0));
+                try {
+                    Object o = ((ForgeConfigSpec.ConfigValue<?>)CubicThings.Config.class.getField(key).get(null)).get();
+                    String outputBase = StringUtils.repeat(' ', depth * 4) + pathName + "\u00A77 = ";
+                    if (o instanceof String)
+                        textListViewConsumer.accept(newEntry.apply(outputBase + "\u00A72\"" + ((String) o).replace("\\", "\\\\").replace("\"", "\\\"") + "\"", 0xDB7F41));
+                    else if (o instanceof Boolean)
+                        textListViewConsumer.accept(newEntry.apply(outputBase + "\u00A7d" + o, 0xDB7F41));
+                    else if (o instanceof Byte && CubicThings.Config.bytesAsBinary.get())
+                        textListViewConsumer.accept(newEntry.apply(outputBase + "\u00A76" + StringUtils.repeat('0', 8 - Integer.toBinaryString((byte) o & 0xFF).length()) + Integer.toBinaryString((byte) o & 0xFF), 0xDB7F41));
+                    else if (o instanceof Number)
+                        textListViewConsumer.accept(newEntry.apply(outputBase + "\u00A76" + o, 0xDB7F41));
+                    else
+                        textListViewConsumer.accept(newEntry.apply(outputBase + "\u00A7r" + o.toString(), 0xDB7F41));
+                } catch (IllegalAccessException | NoSuchFieldException ignored) { }
+                if (!hideSeparators) textListViewConsumer.accept(newEntry.apply(StringUtils.repeat("----------", depth + 1), 0x4400AA));
+            }
         });
     }
 
-    @Override
+    public void doConfigChange(Map<String, Object> valueMap, Class<?> clazz){
+        doConfigChange(valueMap, clazz, null, 0);
+    }
+
     @SuppressWarnings("unchecked")
+    private void doConfigChange(Map<String, Object> valueMap, Class<?> clazz, @Nullable AbstractConfig nestedConfig, final int depth){
+        String[] splitInput = this.keyInputField.getText().trim().split("\\.");
+        String lastSplit = splitInput[splitInput.length - 1];
+        if (!valueMap.containsKey(lastSplit) && valueMap.values().stream().noneMatch((o) -> o instanceof AbstractConfig)) {
+            this.keyInputField.setTextColor(CubicThings.Config.fail.get());
+            this.invalidKey = true;
+        } else {
+            try {
+                Object value = nestedConfig == null ? valueMap.get(lastSplit) : nestedConfig.get(lastSplit);
+                String valueInput = this.valueInputField.getText().trim();
+                if (value instanceof ForgeConfigSpec.ValueSpec){
+                    // Convert String input into field values.
+                    ForgeConfigSpec.ConfigValue<?> configValue = (ForgeConfigSpec.ConfigValue<?>) clazz.getField(lastSplit).get(null);
+                    Class<?> valueClazz = ((ForgeConfigSpec.ValueSpec) value).getClazz();
+                    if (valueClazz == String.class) {
+                        ((ForgeConfigSpec.ConfigValue<String>) configValue).set(valueInput);
+                    } else if (valueClazz == Byte.class) {
+                        int intValue = Integer.decode(valueInput);
+                        if (intValue < 255) ((ForgeConfigSpec.ConfigValue<Byte>) configValue).set((byte) intValue);
+                        else throw new Exception("Bad byte value");
+                    } else if (valueClazz == Short.class) {
+                        ((ForgeConfigSpec.ConfigValue<Short>) configValue).set(Short.decode(valueInput));
+                    } else if (valueClazz == Integer.class) {
+                        ((ForgeConfigSpec.ConfigValue<Integer>) configValue).set(Integer.decode(valueInput));
+                    } else if (valueClazz == Float.class) {
+                        ((ForgeConfigSpec.ConfigValue<Float>) configValue).set(Float.valueOf(valueInput));
+                    } else if (valueClazz == Double.class) {
+                        ((ForgeConfigSpec.ConfigValue<Double>) configValue).set(Double.valueOf(valueInput));
+                    }
+                    else if (valueClazz == Boolean.class) {
+                        boolean isTruthy = valueInput.equalsIgnoreCase("true") || valueInput.equalsIgnoreCase("yes") || valueInput.equalsIgnoreCase("y") || valueInput.equalsIgnoreCase("t") || (StringUtils.isNumeric(valueInput) && Byte.parseByte(valueInput) == 1);
+                        boolean isFalsy = valueInput.equalsIgnoreCase("false") || valueInput.equalsIgnoreCase("no") || valueInput.equalsIgnoreCase("n") || valueInput.equalsIgnoreCase("f") || (StringUtils.isNumeric(valueInput) && Byte.parseByte(valueInput) == 0);
+                        if (isTruthy || isFalsy) ((ForgeConfigSpec.ConfigValue<Boolean>) configValue).set(isTruthy && !isFalsy);
+                        else throw new Exception("Bad boolean value");
+                    }
+
+                    // Save changes.
+                    configValue.save();
+                    CubicThings.Config.SPEC.save();
+                    this.foundKey = true;
+                    this.invalidKey = false;
+                    this.valueInputField.setTextColor(CubicThings.Config.good.get());
+                } else if (valueMap.get(splitInput[depth]) instanceof AbstractConfig) {
+                    // Call same function if next value in path translates to an AbstractConfig
+                    AbstractConfig abstractConfig = (AbstractConfig) valueMap.get(splitInput[depth]);
+                    doConfigChange(abstractConfig.valueMap(), clazz, abstractConfig, depth + 1);
+                    if (!this.foundKey) {
+                        this.invalidKey = true;
+                        throw new Exception();
+                    }
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e){
+                // Do bad things
+                if (!valueMap.containsKey(lastSplit) || (nestedConfig != null && nestedConfig.get(lastSplit) == null)){
+                    this.invalidKey = true;
+                    this.foundKey = false;
+                    this.keyInputField.setTextColor(CubicThings.Config.fail.get());
+                } else {
+                    this.invalidValue = true;
+                    this.valueInputField.setTextColor(CubicThings.Config.fail.get());
+                }
+                CubicThings.LOGGER.info("ConfigScreen Value Error: " + e);
+            }
+        }
+    }
+
+    @Override
     protected void init() {
         super.init();
         getMinecraft().keyboardListener.enableRepeatEvents(true);
 
-        this.keyInputField = new TextFieldWidget(this.font, this.width / 2 - 85, 30, 80, 10, ITextComponent.getTextComponentOrEmpty("Key"));
+        this.keyInputField = new TextFieldWidget(this.font, this.width / 2 - 135, 30, 130, 10, ITextComponent.getTextComponentOrEmpty("Key"));
         this.keyInputField.setMaxStringLength(256);
         this.keyInputField.setTextColor(0x888888);
         this.keyInputField.setText("Key");
@@ -119,40 +215,8 @@ public class ConfigScreen extends Screen implements ITextListHolder {
         }));
 
         this.submitChangeButton = addButton(new Button(this.width / 2 + 100, 25, 50, 20,  new TranslationTextComponent("gui.submit"), (button) -> {
-            if (!CubicThings.Config.SPEC.valueMap().containsKey(this.keyInputField.getText())) {
-                this.keyInputField.setTextColor(0xbb4444);
-                this.invalidKey = true;
-            } else {
-                ForgeConfigSpec.ValueSpec value = (ForgeConfigSpec.ValueSpec) CubicThings.Config.SPEC.valueMap().get(this.keyInputField.getText());
-                try {
-                    Object o = CubicThings.Config.class.getField(this.keyInputField.getText()).get(null);
-                    String valueInput = this.valueInputField.getText();
-                    if (value != null && o instanceof ForgeConfigSpec.ConfigValue<?>){
-                        if (value.getClazz() == String.class)
-                            ((ForgeConfigSpec.ConfigValue<String>) o).set(valueInput);
-                        else if (value.getClazz() == Integer.class)
-                            ((ForgeConfigSpec.ConfigValue<Integer>) o).set(Integer.decode(valueInput));
-                        else if (value.getClazz() == Double.class)
-                            ((ForgeConfigSpec.ConfigValue<Double>) o).set(Double.valueOf(valueInput));
-                        else if (value.getClazz() == Boolean.class) {
-                            boolean isTruthy = valueInput.equalsIgnoreCase("true") || valueInput.equalsIgnoreCase("yes") || valueInput.equalsIgnoreCase("y") || valueInput.equalsIgnoreCase("t") || valueInput.equals("1");
-                            boolean isFalsy = valueInput.equalsIgnoreCase("false") || valueInput.equalsIgnoreCase("no") || valueInput.equalsIgnoreCase("n") || valueInput.equalsIgnoreCase("f") || valueInput.equals("0");
-                            if (isTruthy || isFalsy) ((ForgeConfigSpec.ConfigValue<Boolean>) o).set(isTruthy && !isFalsy);
-                            else throw new Exception();
-                        }
-
-                        ((ForgeConfigSpec.ConfigValue<?>) o).save();
-                        CubicThings.Config.SPEC.save();
-                        this.valueInputField.setTextColor(0x44BA44);
-                    } else {
-                        throw new Exception();
-                    }
-                } catch (Exception e){
-                    this.valueInputField.setTextColor(0xbb4444);
-                    this.invalidValue = true;
-                }
-            }
-
+            // Change a config value with a given input.
+            doConfigChange(CubicThings.Config.SPEC.valueMap(), CubicThings.Config.class);
         }));
 
         this.configList = new TextListWidget(this, 300,150,this.width / 2 - 150,50,4, 4, this.font.FONT_HEIGHT + 2, 0xC0C0C0, true, true);
